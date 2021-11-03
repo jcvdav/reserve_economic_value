4a4ccaef966b1e1f15f9a4e9503aa2fcd2d2b0d5
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-11-02 21:02:42 -0700

    Updated analysis

--- scripts/02_analysis/02_biomass_and_value_by_community.R
+++ scripts/02_analysis/02_biomass_and_value_by_community.R
@@ -22,8 +22,8 @@ family <-
 prices <-
   read_csv(file = here("data", "processed_data", "family_prices.csv"))
 costs <-
-  read.csv(here("data", "raw_data", "reserve_cost_per_hectare.csv")) %>% 
-  mutate(cost_mxp_ha = cost_usd_ha * 19.2472) %>% 
+  read.csv(here("data", "raw_data", "reserve_cost_per_hectare.csv")) %>%
+  mutate(cost_mxp_ha = cost_usd_ha * 19.2472) %>%
   select(community, cost_mxp_ha)
 
 fish_biomass_value <- fish_biomass %>%
@@ -31,39 +31,51 @@ fish_biomass_value <- fish_biomass %>%
   left_join(family, by = c("species", "genus")) %>%
   left_join(prices, by = c("family", "group")) %>%
   mutate(value_mxp_hect = mean_price * biomass_kg_hect) %>%
-  select(-c(genus,species))
+  select(-c(genus, species))
 
 invert_biomass_value <- invert_biomass %>%
   # filter(biomass_kg_hect > 0) %>%
   left_join(prices, by = c("family", "group")) %>%
-  mutate(value_mxp_hect = mean_price * biomass_kg_hect) 
+  mutate(value_mxp_hect = mean_price * biomass_kg_hect)
 
-biomass_value <- rbind(fish_biomass_value, invert_biomass_value) 
+biomass_value <- rbind(fish_biomass_value, invert_biomass_value)
 
-biomass_by_community <- biomass_value %>%
+biomass_by_transect <- biomass_value %>%
   drop_na(mean_price) %>%
   group_by(community, site, zone, group, transect) %>%
   summarize(
     biomass_kg_hect = sum(biomass_kg_hect, na.rm = T),
-    value_mxp_hect = sum(value_mxp_hect, na.rm = T) / 1000
-  ) %>%
+    value_mxp_hect = sum(value_mxp_hect, na.rm = T) / 1000)
+
+biomass_by_community <- biomass_by_transect %>% 
   group_by(community, zone, group) %>%
   summarize(
     biomass_kg_hect_sd = sd(biomass_kg_hect, na.rm = T),
     biomass_kg_hect = mean(biomass_kg_hect, na.rm = T),
-    value_mxp_hect_sd = sd(value_mxp_hect, na.rm = T),
+    value_mxp_hect_ci25 = quantile(value_mxp_hect, probs = 0.25, na.rm = T),
+    value_mxp_hect_ci75 = quantile(value_mxp_hect, probs = 0.75, na.rm = T),
     value_mxp_hect = mean(value_mxp_hect)
   ) %>%
   ungroup() %>%
   mutate(community = fct_reorder(community, value_mxp_hect, sum))
 
 
-tot_val <- biomass_by_community %>%
-  filter(zone == "Reserva") %>%
-  ggplot(aes(x = community, y = value_mxp_hect, fill = group)) +
-  geom_errorbar(aes(ymin = value_mxp_hect, ymax = value_mxp_hect + value_mxp_hect_sd),
-                position = "dodge") +
+tot_val_data <- biomass_by_community %>%
+  filter(zone == "Reserva")
+
+
+
+# FIGURES
+
+# Total value
+
+tot_val <-
+  ggplot(data = tot_val_data,
+         mapping = aes(x = community, y = value_mxp_hect, fill = group)) +
   geom_col(color = "black", position = "dodge") +
+  geom_errorbar(aes(ymin = value_mxp_hect - value_mxp_hect_ci25,
+                    ymax = value_mxp_hect + value_mxp_hect_ci75),
+                position = "dodge") +
   scale_fill_brewer(palette = "Set1", direction = -1) +
   theme_bw() +
   coord_flip() +
@@ -74,6 +86,22 @@ tot_val <- biomass_by_community %>%
     legend.position = c(1, 0),
     legend.background = element_blank()
   )
+
+
+tot_val_dist <- biomass_by_transect %>%
+  filter(zone == "Reserva") %>% 
+  ggplot(aes(y = community, x = value_mxp_hect, fill = group)) +
+  geom_density_ridges(bandwidth = 10, alpha = 0.75) +
+  theme_bw() +
+  labs(x = "Valor (1,000 MXP / ha)", y = "Comunidad") +
+  scale_fill_brewer(palette = "Set1", direction = -1) +
+  guides(fill = guide_legend("Grupo"))
+
+
+
+
+
+# Sustainable value
 
 sust_val <- biomass_by_community %>%
   select(community, group, zone, value_mxp_hect) %>%
@@ -93,18 +121,26 @@ sust_val <- biomass_by_community %>%
     legend.background = element_blank()
   )
 
+
+
+# Compared to cost
+# 
 biomass_by_community %>%
   select(community, group, zone, value_mxp_hect) %>%
   pivot_wider(names_from = zone, values_from = value_mxp_hect) %>%
   mutate(Res50 = 0.5 * Reserva,
-         diff = Reserva - min(Control, Res50)) %>% 
-  group_by(community) %>% 
-  summarize(value = sum(diff)) %>% 
-  left_join(costs, by = "community") %>% 
-  knitr::kable(col.names = c("Comunidad", "Valor (MXP / hac)", "Costo (MXP / ha)"),
-               format = "latex") %>% 
+         diff = Reserva - min(Control, Res50)) %>%
+  group_by(community) %>%
+  summarize(value = sum(diff)) %>%
+  left_join(costs, by = "community") %>%
+  knitr::kable(
+    col.names = c("Comunidad", "Valor (MXP / hac)", "Costo (MXP / ha)"),
+    format = "latex"
+  ) %>%
   cat(file = here("results", "tab", "valor_y_costo.tex"))
 
+
+# Export figures
 
 ggsave(
   tot_val,
@@ -113,6 +149,12 @@ ggsave(
   height = 4
 )
 
+ggsave(
+  tot_val_dist,
+  filename = here("results", "img", "distribution_total_value_per_hectare.pdf"),
+  width = 8,
+  height = 4
+)
 
 ggsave(
   sust_val,
