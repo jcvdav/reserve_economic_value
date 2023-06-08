eec55a054e562bc4fa31b6764739f82eac3eb17a
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2023-06-08 14:11:32 -0700

    Updated the new figure

--- scripts/03_figures_and_tables/fig_s5_alternative_values.R
+++ scripts/03_figures_and_tables/fig_s5_alternative_values.R
@@ -13,6 +13,8 @@
 ## SET UP ######################################################################
 
 # Load packages ----------------------------------------------------------------
+library(here)
+library(tidyverse)
 
 # Load data --------------------------------------------------------------------
 tot_val_ref <- readRDS(here("data", "output_data", "tot_val_ref.rds")) %>%
@@ -23,7 +25,18 @@ tot_val_ref2 <- readRDS(here("data", "output_data", "tot_val_ref_reviewer.rds"))
 ## PROCESSING ##################################################################
 
 # X ----------------------------------------------------------------------------
-combined <- left_join(a, b, by = c("community", "group"))
+combined <- left_join(tot_val_ref,
+                      tot_val_ref2,
+                      by = c("community", "group"))
+
+model <- lm(ref_value_MXN_hect2 ~ ref_value_MXN_hect, data = combined) %>% 
+  broom::tidy()
+
+fml <- paste0("y(x) = ",
+              round(model$estimate[1], 2),
+              " + ",
+              round(model$estimate[2], 2),
+              "x\n R^2 = 0.901\n F(1, 16) = 146.8\n p < 0.001")
 
 ## VISUALIZE ###################################################################
 
@@ -31,17 +44,22 @@ combined <- left_join(a, b, by = c("community", "group"))
 alternative_values <- ggplot(
   data = combined,
   mapping = aes(x = ref_value_MXN_hect,
-                y = ref_value_MXN_hect2,
-                color = community,
-                shape = group)) +
-  geom_point(size = 3) +
+                y = ref_value_MXN_hect2)) +
+  geom_smooth(method = "lm", color = "red", linetype = "dashed") +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
-  scale_color_brewer(palette = "Set1") +
+  geom_text(x = 25, y = 5, label = fml, inherit.aes = F, size = 3) +
+  geom_point(mapping = aes(fill = community,
+                           shape = group),
+             color = "black",
+             size = 3) +
+  scale_fill_brewer(palette = "Set1") +
+  scale_shape_manual(values = c(21, 24)) +
   labs(x = "Reference value based on historical minimum\n(MXP/ha)",
        y = "Reference value based on second smalles value\n(MXP/ha)",
-       color = "Community",
+       fill = "Community",
        shape = "Group") +
-  guides(color = guide_legend(ncol = 3),
+  guides(fill = guide_legend(ncol = 3,
+                             override.aes = list(shape = 21)),
          shape = guide_legend(ncol = 2)) +
   theme(legend.justification = c(0, 1),
         legend.position = c(0, 1))
@@ -49,5 +67,5 @@ alternative_values <- ggplot(
 ggsave(
   plot = alternative_values,
   filename = here("results", "img", "fig_s5_alternative_values.png"),
-  width = 5,
-  height = 5)
+  width = 5.5,
+  height = 5.5)
